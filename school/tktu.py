47fc9461e30e6891bb1a5a695ec1233afe4fb8b6
rudnesh <88377468+rudnesh@users.noreply.github.com>
2022-08-25 08:28:27 +0530

    Add files via upload

--- school/tktu.py
+++ school/tktu.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from turtle import bgcolor
-root = Tk()
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0,activeforeground="white",fg="white" )
+p = Tk()
+p.title("Media notes")
+menubar = Menu(p)
+filemenu = Menu(menubar, tearoff=0,activeforeground="#837f7f",fg="white" )
 
 filemenu.add_command(label="New",activebackground="#050509",activeforeground="white",command=None)
 filemenu.add_command(label="Open", command=None)
@@ -13,7 +14,6 @@ filemenu.add_command(label="Close", command=None)
 filemenu.add_separator()
 
 menubar.add_cascade(label="File",activebackground = 'white' , menu=filemenu)
-
-
-root.config(menu=menubar)
-root.mainloop()
+#https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/menu-coptions.html
+p.config(menu=menubar)
+p.mainloop()
